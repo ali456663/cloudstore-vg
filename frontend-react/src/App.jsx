51a1a3b00045835c7cc9d18e35bbc6b909595410
Ali Wafa <ali.wafa17943@gmail.com>
2026-06-06 20:24:52 +0200

    Use Amplify proxy paths for frontend APIs

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const PRODUCT_SERVICE_URL = import.meta.env.VITE_PRODUCT_SERVICE_URL
-  || 'http://cloudstore-product-service-env.eba-izkz2b3g.eu-north-1.elasticbeanstalk.com'
+  || '/product-api'
 const USER_ORDER_SERVICE_URL = import.meta.env.VITE_USER_ORDER_SERVICE_URL
-  || 'http://cloudstore-user-order-service.eu-north-1.elasticbeanstalk.com'
+  || '/order-api'
 
 const colorOptions = ['Black', 'White', 'Blue', 'Green']
 const sizeOptions = ['S', 'M', 'L', 'XL']
